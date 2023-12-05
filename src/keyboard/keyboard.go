8a75a7c526c1ab6ddc264a70d5746f5d828a8e30
Gangmyelf <pshirokov731@yandex.ru>
2023-12-05 22:59:32 +0300

    second commit for keyboard with func Average

--- src/keyboard/keyboard.go
+++ src/keyboard/keyboard.go
@@ -3,6 +3,7 @@ package keyboard
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
@@ -22,4 +23,13 @@ func GetFloat() (float64, error) {
 		return 0, err
 	}
 	return finalInput, nil
+}
+
+func Average(numbers []float64) {
+	var sum float64 = 0
+	for _, value := range numbers {
+		sum += value
+	}
+	sampleCount := float64(len(numbers))
+	fmt.Printf("Average: %0.2f\n ", sum/sampleCount)
 }
